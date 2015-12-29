440a9ec0c9ea9e34c3d1b7571937c2edcaa7bede
jiedan <lxb429@gmail.com>
2015-12-29 16:52:54 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,22 +2,16 @@
 
 set -e
 
-if [ "$1" = 'sentry' ]; then
-	defaultConf='/home/sentry/.sentry/sentry.conf.py'
-	linksConf='/etc/sentry/sentry.conf.py'
-	
-	if [ ! -s "$defaultConf" ]; then
-		sentry init "$defaultConf"
-	fi
-	
-	line="execfile('$linksConf')"
-	if ! grep -q "$line" "$defaultConf"; then
-		echo "$line" >> "$defaultConf"
-	fi
-	
-	# TODO sentry upgrade?
-	# it requires stdin to answer questions and dies on 'unexpected EOF'
-	# but does not ask again when run a second time
+# first check if we're passing flags, if so
+# prepend with sentry
+if [ "${1:0:1}" = '-' ]; then
+	set -- sentry "$@"
 fi
+
+case "$1" in
+	celery|cleanup|config|createuser|devserver|django|export|help|import|init|repair|shell|start|upgrade)
+		set -- sentry "$@"
+	;;
+esac
 
 exec "$@"
